283c4fe81cd7a4699f8c90d37b1db257888efb64
Christina Ananth <cjothia@gmail.com>
2021-10-06 01:10:21 -0400

    tetomino moves down

--- client/tetris/script.js
+++ client/tetris/script.js
@@ -44,14 +44,13 @@ document.addEventListener('DOMContentLoaded', () => {
   const theTetrominoes = [lTetromino, zTetromino, tTetromino, oTetromino, iTetromino];
 
 let currentPosition = 4;
+let currentRotation = 0;
 
 //randomly select a Tetromino and its first rotation 
+let random = Math.floor(Math.random() * theTetrominoes.length);
+let current = theTetrominoes[random][currentRotation];
 
-
-
-let current = theTetrominoes[0][0];
-
-//draw the current rotation in the first tetromino
+//draw the tetromino
 
 function draw() {
     current.forEach(index => {
@@ -59,6 +58,40 @@ function draw() {
     });
 }
 
-draw();
+//undraw the tetromino
+function undraw() {
+    current.forEach(index => {
+        squares[currentPosition + index].classList.remove('tetromino');
+    });
+}
+
+//make the tetromino move down every second
+timerId = setInterval(moveDown, 1000);
+
+//move down funtin
+function moveDown() {
+    undraw();
+    currentPosition += width;
+    draw();
+    freeze();
+}
+
+//freeze funtion
+function freeze() {
+    if (current.some(index => squares[currentPosition + index + width].classList.contains('taken'))) {
+        current.forEach(index => squares[currentPosition + index].classList.add('taken'));
+        //start a new tetromino falling
+        random = Math.floor(Math.random() * theTetrominoes.length);
+        current = theTetrominoes[random][currentRotation];
+        currentPosition = 4;
+        draw();
+    }
+}
+
+//move the tetrominoleft, unless it is at the edge or there is a blockage
+
+
+
+
 
 });
